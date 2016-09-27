bae79dcbaea9bba373c382fc433a1a203154bfc4
Bugin10 <ben.axford@hotmail.com>
2016-09-27 18:26:33 +0930

    test

--- hello.py
+++ hello.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python
 print "Content-type: text/html\n\n"
 print "<h1>Hello World</h1>"
+import sqlite3
+
+conn=sqlite3.connect('templog.db')
+curs=conn.cursor()
+
+print "<table>"
+rows=curs.execute("SELECT * FROM temps WHERE timestamp>datetime('now','-100000000 hour', 'localtime') AND timestamp<=datetime('now', 'localtime')")
+#    rows=curs.execute("SELECT * FROM temps WHERE timestamp>datetime('2013-09-19 21:30:02','-1 hour') AND timestamp<=datetime('2013-09-19 $
+for row in rows:
+	rowstr="<tr><td>{0}&emsp;&emsp;</td><td>{1}C&emsp;&emsp;</td><td>{2}C</td></tr>".format(str(row[0]),str(row[1]),str(row[2]))
+	print rowstr
+print "</table>"
+
--- tempsenssql.py
+++ tempsenssql.py
@@ -1,4 +1,6 @@
 import sqlite3
+import sys
+import Adafruit_DHT
 from w1thermsensor import W1ThermSensor
 import json
 import time
@@ -13,23 +15,25 @@ for sensor in W1ThermSensor.get_available_sensors():
 	i = i+1
 
 # the main sensor reading and plotting loop
-while True:
-   currtime = datetime.datetime.now()
-   print(currtime)
-   conn=sqlite3.connect('templog.db')
-   curs=conn.cursor()
-   temps = [0,0]
-   for count in range(0,i):       
-      	 # temperature in celsius
-       	 temp_C = sensors[count].get_temperature()
-      	 # convert celsius to fahrenheit
-      	 temp_F = sensors[count].get_temperature(W1ThermSensor.DEGREES_F)
-      	 # show only one decimal place for temperature
-      	 temp_C = "%.1f" % temp_C
-      	 # write the data to plotly
-         temps[count] = temp_C
-   print(temps[0])
-   curs.execute("INSERT INTO temps values(datetime('now','localtime'), (?), (?))", ( temps[0], temps[1],))
-   conn.commit() 
-   time.sleep(300)
+
+currtime = datetime.datetime.now()
+print(currtime)
+conn=sqlite3.connect('templog.db')
+curs=conn.cursor()
+temps = [0,0]
+for count in range(0,i):       
+	# temperature in celsius
+  	temp_C = sensors[count].get_temperature()
+    	# convert celsius to fahrenheit
+      	temp_F = sensors[count].get_temperature(W1ThermSensor.DEGREES_F)
+      	# show only one decimal place for temperature
+  	temp_C = "%.1f" % temp_C
+   	# write the data to plotly
+      	temps[count] = temp_C
+print(temps[0])
+HIn, TIn = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 17)
+HOu, TOu = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 27)
+curs.execute("INSERT INTO temps values(datetime('now','localtime'), (?), (?), (?), (?))", ( temps[0], temps[1], HIn, HOu,))
+conn.commit() 
+
 
